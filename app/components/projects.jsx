9a37188fffef5f06b1ac73b4f22e39350e456c8f
joesatriani10 <joesatriani10@gmail.com>
2024-04-10 18:39:57 -0700

    update design

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -1,16 +1,17 @@
 'use client';
 import styles from "@/app/page.module.css";
 import css from "./projects.module.css";
+import glow from "../glowEffect.module.css" ;
 import foodPic from "../images/foodapp.png";
 import Image from "next/image";
 
 const Projects = () => {
     return (
-        <section className={styles.section}>
-            <h2>Projects</h2>
+        <section className={`${styles.section} ${glow.glowBlueHoverProjects}`}>
+            <h1 className={css.title}>Projects</h1>
             <div className={css.grid}>
-                <a href="https://food-next-js-seven.vercel.app/" className={css.card} aria-label="Link to Project 1" target="_blank" rel="noopener noreferrer">
-                    <h3>Food App</h3>
+                <a href="https://food-next-js-seven.vercel.app/" className={`${css.card} ${glow.glowPinkHover}`} aria-label="Link to Project 1" target="_blank" rel="noopener noreferrer">
+                    <h2>Food App [Next.js]</h2>
                     <Image
                         className={css.image}
                         src={foodPic}
@@ -22,17 +23,17 @@ const Projects = () => {
                     />
                 </a>
 
-                <a href="/project2" className={css.card} aria-label="Link to Project 2">
+                <a href="/project2" className={`${css.card} ${glow.glowPinkHover}`} aria-label="Link to Project 2">
                     <h3>Project 2</h3>
                     <p>Description of Project 2</p>
                 </a>
 
-                <a href="/project3" className={css.card} aria-label="Link to Project 3">
+                <a href="/project3" className={`${css.card} ${glow.glowPinkHover}`} aria-label="Link to Project 3">
                     <h3>Project 3</h3>
                     <p>Description of Project 3</p>
                 </a>
 
-                <a href="/project4" className={css.card} aria-label="Link to Project 4">
+                <a href="/project4" className={`${css.card} ${glow.glowPinkHover}`} aria-label="Link to Project 4">
                     <h3>Project 4</h3>
                     <p>Description of Project 4</p>
                 </a>
