9d6928ec0ae4106482031425db35623dbbd25e42
joesatriani10 <joesatriani10@gmail.com>
2024-04-11 14:02:44 -0700

    Removed gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // basePath: "/portfolio",
-    // output: "export",  // <=== enables static exports
-    // reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
