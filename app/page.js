376d7ceffafa113cf4edd8644776202f3df635c9
joesatriani10 <joesatriani10@gmail.com>
2024-04-11 12:06:12 -0700

    fixed metadata

--- app/page.js
+++ app/page.js
@@ -3,11 +3,6 @@ import Header from "@/app/components/header";
 import Profile from "@/app/components/profile";
 import Skills from "@/app/components/skills";
 
-export const metadata = {
-  title: "Angel | Personal Portfolio",
-  description: "Angel is a full-stack developer with 7 years of experience in many technologies.",
-};
-
 export default function Home() {
     return (
         <main className="p-8 max-w-4xl mx-auto bg-gray-900 text-white">
