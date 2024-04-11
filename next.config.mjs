7f04f0f672253bb644366307fa64c4c59c16ed9b
joesatriani10 <joesatriani10@gmail.com>
2024-04-10 20:28:47 -0700

    gh-pages config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/portfolio",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+};
 
 export default nextConfig;
