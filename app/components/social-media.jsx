38270d3bb80d24e89deb9f3a1cea996511ab758a
joesatriani10 <joesatriani10@gmail.com>
2024-04-20 01:36:37 -0700

    updated icon style

--- app/components/social-media.jsx
+++ app/components/social-media.jsx
@@ -3,17 +3,19 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import {useMediaQuery} from "@mui/material";
+import styles from './social-media.module.css';
+
 
 const SocialMedia = () => {
 
     const matches = useMediaQuery('(max-width:768px)');
     return (
         <>
-            <a href="https://www.linkedin.com/in/angel-israel-real-montes-de-oca/" target="_blank" rel="noreferrer">
+            <a className={styles.socialMediaLink} href="https://www.linkedin.com/in/angel-israel-real-montes-de-oca/" target="_blank" rel="noreferrer">
                 <LinkedInIcon sx={{fontSize: matches ? 25 : 40}}/> </a>
-            <a href="https://github.com/joesatriani10" target={"_blank"} rel="noreferrer"><GitHubIcon
+            <a className={styles.socialMediaLink} href="https://github.com/joesatriani10" target={"_blank"} rel="noreferrer"><GitHubIcon
                 sx={{fontSize: matches ? 25 : 40}}/></a>
-            <a href="https://www.youtube.com/@AngelMontesdeOca" target={"_blank"}
+            <a className={styles.socialMediaLink} href="https://www.youtube.com/@AngelMontesdeOca" target={"_blank"}
                rel="noreferrer"><YouTubeIcon sx={{fontSize: matches ? 25 : 40}}/></a>
         </>
     );
