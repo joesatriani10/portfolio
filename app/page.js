d459079d7c5d49631396e5fafc98f7884b2ffbb2
joesatriani10 <joesatriani10@gmail.com>
2024-04-20 00:54:15 -0700

    added footer to Home

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Header from "@/app/components/header";
 import Profile from "@/app/components/profile";
 import Skills from "@/app/components/skills";
 import Experience from "@/app/components/experience";
+import Footer from "@/app/components/footer";
 
 export default function Home() {
     return (
@@ -12,6 +13,7 @@ export default function Home() {
             <Skills/>
             <Experience/>
             <Projects/>
+            <Footer/>
         </main>
     );
 }
