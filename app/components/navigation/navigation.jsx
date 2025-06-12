ce03676c22da0e799098ad1f1474d9ac6a6fd25f
Angel Montes de Oca <joesatriani10@gmail.com>
2025-06-11 22:26:46 -0700

    Add navigation with client-side routing

---
+++ app/components/navigation/navigation.jsx
@@ -0,0 +1,15 @@
+'use client';
+import Link from 'next/link';
+import styles from './navigation.module.css';
+
+const Navigation = () => {
+    return (
+        <nav className={styles.navbar}>
+            <Link href="/" className={styles.link}>Home</Link>
+            <Link href="/experience" className={styles.link}>Experience</Link>
+            <Link href="/projects" className={styles.link}>Projects</Link>
+        </nav>
+    );
+};
+
+export default Navigation;
---
+++ app/projects/page.js
@@ -0,0 +1,9 @@
+import Projects from "@/app/components/projects/projects";
+
+export default function ProjectsPage() {
+    return (
+        <main className="p-8 max-w-4xl mx-auto">
+            <Projects />
+        </main>
+    );
+}
