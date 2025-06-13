1634e450d830f31cb8b493bd4f8d73a5a9316ffc
Angel Montes de Oca <joesatriani10@gmail.com>
2025-06-13 02:13:57 -0700

    updated: look & feel

--- app/components/job-experience/job-experience.jsx
+++ app/components/job-experience/job-experience.jsx
@@ -6,7 +6,7 @@ const JobExperience = ({ title, company, location, dates, responsibilities }) =>
     return (
         <div className={styles.jobExperience}>
             <h4><strong>{title}</strong> at {company} - {location} ({dates})</h4>
-            <ul>
+            <ul className={styles.noBullets}>
                 {responsibilities.map((responsibility, index) => (
                     <li key={index} className={styles.responsibilityItem}>{responsibility}</li>
                 ))}
