8de5deb9f1f1dc99febb5ab42c98a4e0d00c320b
Angel Montes de Oca <joesatriani10@gmail.com>
2025-12-05 17:01:19 -0800

    chore: added metadata

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,30 @@
 import './global.css';
-import { Inter } from "next/font/google";
-import Divider from "@/app/components/divider/divider";
-import Footer from "@/app/components/footer/footer";
-import Header from "@/app/components/header/header";
-import DarkModeToggle from "@/app/components/dark-mode-toggle/dark-mode-toggle";
+import { Inter } from 'next/font/google';
+import Divider from '@/app/components/divider/divider';
+import Footer from '@/app/components/footer/footer';
+import Header from '@/app/components/header/header';
+import DarkModeToggle from '@/app/components/dark-mode-toggle/dark-mode-toggle';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+    title: 'Angel Montes | Portfolio',
+    description: 'Software engineer portfolio',
+    icons: {
+        icon: '/favicon.png',
+    },
+};
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-        <head>
-            <link rel="icon" href="/favicon.png" type="image/png" />
-        </head>
         <body className={inter.className}>
         <Header />
         <Divider />
         <DarkModeToggle />
+
         {children}
+
         <Divider />
         <div className="p-8 max-w-4xl mx-auto bg-[#D3D3D3] text-black">
             <Footer />
